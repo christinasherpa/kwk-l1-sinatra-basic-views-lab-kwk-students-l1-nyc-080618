3d4b344698fa5239bfda97e32bdf47608df73df1
christinasherpa <chrisher24@charter.newvisions.org>
2018-08-15 13:47:42 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,5 +2,28 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+get '/' do
+  erb :index
+end
+
+get '/marnie' do
+  erb :marnie
+end
+
+get '/jevina' do
+  erb :jevina
+end
+
+get '/christina' do
+  erb :christina
+end
+
+get '/wendy' do
+  erb :wendy
+end
+
+get '/leyla' do
+  erb :leyla
+end
 
 end
